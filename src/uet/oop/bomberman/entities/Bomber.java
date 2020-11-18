982767ffe852d41d4019eec1e7a59731cf544481
minhtuyen <minhtuyen300920011@gmail.com>
2020-11-18 11:41:56 +0700

    commit message

---
+++ src/uet/oop/bomberman/entities/Bomber.java
@@ -0,0 +1,25 @@
+package uet.oop.bomberman.entities;
+
+import uet.oop.bomberman.graphics.Screen;
+import uet.oop.bomberman.graphics.Sprite;
+
+public class Bomber extends Entity {
+
+    public Bomber(int x, int y,  Sprite _sprite) {
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void render(Screen screen) {
+
+    }
+
+    @Override
+    public boolean collide(Entity e) {
+        return false;
+    }
+}
---
+++ src/uet/oop/bomberman/entities/Wall.java
@@ -0,0 +1,25 @@
+package uet.oop.bomberman.entities;
+
+import uet.oop.bomberman.graphics.Screen;
+import uet.oop.bomberman.graphics.Sprite;
+
+public class Wall extends Entity {
+
+    public Wall(int x, int y, Sprite _sprite) {
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void render(Screen screen) {
+
+    }
+
+    @Override
+    public boolean collide(Entity e) {
+        return false;
+    }
+}
