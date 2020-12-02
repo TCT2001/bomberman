9624e5e65d8c7be7105a0f62c8f523c5363a07e4
minhtuyen <minhtuyen300920011@gmail.com>
2020-12-02 22:25:01 +0700

    adding css

--- src/uet/oop/bomberman/graphics/fxml/SettingController.java
+++ src/uet/oop/bomberman/graphics/fxml/SettingController.java
@@ -1,13 +1,18 @@
 package uet.oop.bomberman.graphics.fxml;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -33,6 +38,8 @@ public class SettingController implements Initializable {
     Button btnBoom;
     @FXML
     Button btnSave;
+    @FXML
+    Button btnReturn;
 
     Button button;
 
@@ -55,35 +62,40 @@ public class SettingController implements Initializable {
         FileUltis.setKeyNav(s);
     }
 
-    public void up(MouseEvent mouseEvent) {
+    public void up() {
         i = 0;
         button = btnUp;
         getKey();
     }
 
-    public void left(MouseEvent mouseEvent) {
+    public void left() {
         i = 1;
         button = btnLeft;
         getKey();
     }
 
-    public void right(MouseEvent mouseEvent) {
+    public void right() {
         i = 3;
         button = btnRight;
         getKey();
     }
 
-    public void down(MouseEvent mouseEvent) {
+    public void down() {
         i = 2;
         button = btnDown;
         getKey();
     }
 
-    public void boom(MouseEvent mouseEvent) {
+    public void boom() {
         i = 4;
         button = btnBoom;
         getKey();
     }
+
+    public void setBtnReturn(MouseEvent mouseEvent) {
+        FxHelper.setScene(mouseEvent, PATH_HOME);
+    }
+
 
     public void getKey() {
         final Stage dialog = new Stage();
@@ -91,15 +103,31 @@ public class SettingController implements Initializable {
         VBox dialogVbox = new VBox(20);
         dialogVbox.setAlignment(Pos.CENTER);
         //set max score
-        Text text = new Text("press any key\n\t....");
-        text.setFont(Font.font("Arial"));
-        dialogVbox.getChildren().add(text);
-        Scene dialogScene = new Scene(dialogVbox, 200, 100);
-        dialogScene.setOnKeyPressed(keyEvent -> {
-            dialog.hide();
-            s[i] = keyEvent.getCode().toString();
-            button.setText(s[i]);
+        Text text = new Text("Press any key\n\t");
+        //CSS text
+        DropShadow ds = new DropShadow();
+        ds.setOffsetY(3.0f);
+        ds.setColor(Color.color(0.4f, 0.4f, 0.4f));
+        text.setEffect(ds);
+        text.setCache(true);
+        text.setX(10.0f);
+        text.setY(270.0f);
+        text.setFill(Color.RED);
+        text.setFont(Font.font(null, FontWeight.BOLD, 32));
 
+        dialogVbox.getChildren().add(text);
+        Scene dialogScene = new Scene(dialogVbox, 300, 200);
+        dialogScene.setOnKeyPressed(keyEvent -> {
+            text.setText(keyEvent.getCode().toString());
+            text.setStyle("");
+            Button buttonSave = new Button();
+            buttonSave.setText("Save");
+            dialogVbox.getChildren().add(buttonSave);
+            buttonSave.setOnAction(event -> {
+                dialog.hide();
+                s[i] = keyEvent.getCode().toString();
+                button.setText(s[i]);
+            });
         });
         dialog.setTitle("Setting");
         dialog.setScene(dialogScene);
