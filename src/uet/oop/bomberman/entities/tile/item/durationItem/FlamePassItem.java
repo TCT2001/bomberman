1834b36e6982251382b60378fc647e2b00879316
minhtuyen <minhtuyen300920011@gmail.com>
2020-12-08 09:31:29 +0700

    UML diagram

--- src/uet/oop/bomberman/entities/tile/item/durationItem/FlamePassItem.java
+++ src/uet/oop/bomberman/entities/tile/item/durationItem/FlamePassItem.java
@@ -4,13 +4,14 @@ import uet.oop.bomberman.Game;
 import uet.oop.bomberman.entities.tile.item.Item;
 import uet.oop.bomberman.graphics.Sprite;
 
-public class FlamePassItem extends Item {
+public class FlamePassItem extends DurationItem {
     public FlamePassItem(int x, int y, Sprite sprite, int currentLevel) {
         super(x, y, sprite, currentLevel);
     }
 
     @Override
     public void setAttribute() {
+        this.duration = 360;
         hasPower = true;
         Game.setBomberPassFlame(true);
     }
